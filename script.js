20c3dad5e8fd5c46d3f8bb03db2cd06fa23bf502
Gabriel <138737129+Gabriel0991@users.noreply.github.com>
2023-09-06 09:01:10 +1000

    Added player choice
    player choice is case insensitive
    it take a take rock ,paper and scissor string and returns a number between 0 - 2

--- script.js
+++ script.js
@@ -2,15 +2,26 @@
 
 const getComputerChoice = () => {
   let rps = [`rock`, `paper`, `scissor`]
-  let randomNumber = Math.floor(Math.random() * rps.length)
-  let result = rps[randomNumber]
-  return result
+  return (randomNumber = Math.floor(Math.random() * rps.length))
+  // let result = rps[randomNumber]
+  // return result
 }
 console.log(getComputerChoice())
 
-//  Player choice
+//  Player choice case insensitive and convert string to number for ease to compare
 
-const getPlayerChoice = (myChoice) => {
-  if (myChoice = )
-  
+const getPlayerChoice = myChoice => {
+  const choice = myChoice.toLowerCase()
+  if (choice === `rock`) {
+    return 0
+  } else if (choice === `paper`) {
+    return 1
+  } else if (choice === `scissor`) {
+    return 2
+  } else {
+    console.log(`Choose rock,paper or scissor`)
+  }
 }
+console.log(getPlayerChoice(`Rock`))
+
+// Play a single round and declare winner
