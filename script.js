7f96dea561803956096a7335fa0cdba4b12ec66e
Gabriel <138737129+Gabriel0991@users.noreply.github.com>
2023-09-06 09:31:27 +1000

    getPlayerChoice returns rock,paper,scissor

--- script.js
+++ script.js
@@ -12,16 +12,10 @@ console.log(getComputerChoice())
 
 const getPlayerChoice = myChoice => {
   const choice = myChoice.toLowerCase()
-  if (choice === `rock`) {
-    return 0
-  } else if (choice === `paper`) {
-    return 1
-  } else if (choice === `scissor`) {
-    return 2
+  if (choice === `rock` || choice === `paper` || choice === `scissor`) {
+    return choice
   } else {
     console.log(`Choose rock,paper or scissor`)
   }
 }
 console.log(getPlayerChoice(`Rock`))
-
-// Play a single round and declare winner
