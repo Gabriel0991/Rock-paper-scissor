1475ef775dd8f67cc5cd85c2ffda139e7d02d014
Gabriel <138737129+Gabriel0991@users.noreply.github.com>
2023-09-06 08:29:49 +1000

    Computer choice returns a random value

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log(`Hello world`)
+// Computer returns a random result
+
+const getComputerChoice = () => {
+  let rps = [`rock`, `paper`, `scissor`]
+  let randomNumber = Math.floor(Math.random() * rps.length)
+  let result = rps[randomNumber]
+  return result
+}
+console.log(getComputerChoice())
+
+//  Player choice
+
+const getPlayerChoice = (myChoice) => {
+  if (myChoice = )
+  
+}
